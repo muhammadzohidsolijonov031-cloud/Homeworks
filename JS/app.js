c2d2efa237ec85e3dd3b1429874a3dafb8c20418
Muhammadzohid Solijonov <muhammadzohidsolijonov031@gmail.com>
2026-06-17 15:02:56 +0500

    update

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,6 @@
-// function getoddid(arr) {
-//   return arr.filter((prod) => prod.id % 2 === 0);
-// }
-// console.log(getoddid(products));
-
-// const lugat = [
-//   { ar: "Qalamun", uz: "Qalam" },
-//   { ar: "Kitabun", uz: "Kitob" },
-//   { ar: "Maktabun", uz: "Stol" },
-// ];
-
-// lugat.forEach(tarjima >= `arabic: ${lugat.ar} uzbek: ${lugat.uz}`);
-// console.log(lugat);
-
-// Shu yerda forEach ishlating va natijani konsolga chiqaring
 //====================================================================================
 let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 let juftlar = sonlar.filter((n) => n % 2 === 0);
 
 console.log(juftlar);
